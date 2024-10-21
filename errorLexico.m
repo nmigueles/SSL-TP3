85470ab4a6b8812faa38fdf6090149d144fe52a1
nmigueles <nicolasmigueles99@gmail.com>
2024-10-21 20:20:43 -0300

    cambios

---
+++ errorLexico.m
@@ -0,0 +1,9 @@
+// Comentario de una linea
+inicio
+    $1
+    leer (a, b);
+    cc := a + (b - 2);
+    escribir ("Imprime cc:", cc);
+    r := random(a, b);
+    escribir ("Imprime r:", r);
+fin
---
+++ errorSemanticoAsignacion.m
@@ -0,0 +1,6 @@
+// Comentario de una linea
+inicio
+    // Error semantico: a es un identificador, 
+    // por lo que se entiende que quiere asignarle un valor
+    a :=;
+fin
